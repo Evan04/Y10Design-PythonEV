441a71ca5c4331cc49801288a1b76d38ef8a2209
Evan Van <evan.van@dru.local>
2019-09-24 18:41:25 -0400

    VendingMachine.py

---
+++ VendingMachine.py
@@ -0,0 +1,125 @@
+#Vending Machine
+#Evan Van
+#Upper Canada College
+
+print(" ")
+print("		     Welcome To The Vending Machine! ~(˘▾˘~)")
+print(" ")
+print("1. Milk - $2.25")
+print(" ")
+print("2. Apple - $1.25")
+print(" ")
+print("3. Bottled Water - $1.55")
+print(" ")
+print("4. Chicken Wings - $5.75")
+print(" ")
+
+#********************** Variables **********************
+
+varItem = int(input("Please make your selection. \n"))
+
+varMoney = 0
+
+varLost = 0
+
+varPrice1 = 2.25
+
+varPrice2 = 1.25
+
+varPrice3 = 1.55
+
+varPrice4 = 5.75
+
+#*********************** Item #1 ***********************
+
+if varItem == 1:
+	money = float(input("Selected: Milk. Please insert $2.25. \n"))
+
+	if money == 2.25:
+		print("Here Is Your Milk.")
+		print(" ")
+		print("Hope It's Not Expired! ♪~ ᕕ(ᐛ)ᕗ")
+
+	elif money < 2.25:
+		print("Insufficient Funds.")
+		print(" ")
+		print("Your Purchase Has Been Canceled And You Have Lost $"+str(money)+". ༼ つ ಥ_ಥ ༽つ")
+
+	elif money > 2.25:
+
+		varLost = money - 2.25
+
+		print("You have lost $"+str(varLost)+" since the milk only costed $"+str(varPrice1)+".")
+		print(" ")
+		print("Have A Fantastic Day! ლ(´ڡ`ლ)")
+
+#*********************** Item #2 ***********************
+
+elif varItem == 2:
+	money = float(input("Selected: Apple. Please insert $1.25. \n"))
+
+	if money == 1.25:
+		print("Here Is Your Apple.")
+		print(" ")
+		print("Hope It's Not Rotten! ༼ つ ◕_◕ ༽つ")
+
+	elif money < 1.25:
+		print("Insufficient Funds.")
+		print(" ")
+		print("Your Purchase Has Been Canceled And You Have Lost $"+str(money)+". ╚(ಠ_ಠ)=┐")
+
+	elif money > 1.25:
+
+		varLost = money - 1.25
+
+		print("You have lost $"+str(varLost)+" since the apple only costed $"+str(varPrice2)+".")
+		print(" ")
+		print("Have A Fantastic Day! (▰˘◡˘▰)")
+
+#*********************** Item #3 ***********************
+
+elif varItem == 3:
+	money = float(input("Selected: Bottled Water. Please insert $1.55. \n"))
+
+	if money == 1.55:
+		print("Here Is Your Bottled Water.")
+		print(" ")
+		print("Hope It's Not Contaminated! ¯\_(ツ)_/¯") 
+
+	elif money < 1.55:
+		print("Insufficient Funds.")
+		print(" ")
+		print("Your Purchase Has Been Canceled And You Have Lost $"+str(money)+". ( ͡° ͜ʖ ͡°)")
+
+	elif money > 1.55:
+
+		varLost = money - 1.55
+
+		print("You have lost $"+str(varLost)+" since the bottled water only costed $"+str(varPrice3)+".")
+		print(" ")
+		print("Have A Fantastic Day! (ง°ل͜°)ง")
+
+#*********************** Item #4 ***********************
+
+elif varItem == 4:
+	money = float(input("Selected: Chicken Wings. Please insert $5.75. \n"))
+
+	if money == 5.75:
+		print("Here Are Your Chicken Wings.")
+		print(" ")
+		print("Hope It's Fully Cooked! (づ￣ ³￣)づ")
+
+	elif money < 5.75:
+		print("Insufficient Funds.")
+		print(" ")
+		print("Your Purchase Has Been Canceled And You Have Lost $"+str(money)+". ʕ•ᴥ•ʔ")
+
+	elif money > 5.75:
+
+		varLost = money - 5.75
+
+		print("You have lost $"+str(varLost)+" since the chicken wings only costed $"+str(varPrice4)+".")
+		print(" ")
+		print("Have A Fantastic Day! ᕙ(⇀‸↼‶)ᕗ")
+
+input("Press ENTER to quit the program")
